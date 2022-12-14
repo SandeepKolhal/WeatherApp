328fe2b094a258fdc3d705e9cee09fd9f68f7e3c
SandeepKolhal <sandeepkolhal2015@gmail.com>
2022-12-15 02:42:52 +0530

    API_KEY security

--- app/src/main/java/com/android/weatherapp/network/AuthorizationInterceptor.kt
+++ app/src/main/java/com/android/weatherapp/network/AuthorizationInterceptor.kt
@@ -1,5 +1,6 @@
 package com.android.weatherapp.network
 
+import com.android.weatherapp.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -7,7 +8,7 @@ class AuthorizationInterceptor() : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
         val data = request.url.newBuilder().addQueryParameter(
-            "key", "bf6de53ebea24e7a8a171758221312"
+            "key", BuildConfig.API_KEY
         ).build()
 
         request =
